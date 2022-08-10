a07a1cc9fbd4c669f5caa0487d8c70419d941fb6
mongie15 <107944478+mongie15@users.noreply.github.com>
2022-08-10 01:35:58 -0700

    first commit

---
+++ BulletController.js
@@ -0,0 +1,54 @@
+import Bullet from "./bullet.js";
+
+export default class bulletController {
+    bullets = [];
+    timeTillNextBulletAllowed = 0;
+
+    constructor(canvas,maxBulletsAtTime,bulletColor,soundEnabled){
+        this.canvas = canvas;
+        this.maxBulletsAtTime = maxBulletsAtTime;
+        this.bulletColor = bulletColor;
+        this.soundEnabled = soundEnabled;
+
+        this.shootsound = new Audio("soundss/sounds_shoot.wav")
+        this.shootsound.volume = 0.5;
+    }
+
+    draw(ctx) {
+        this.bullets = this.bullets.filter(bullet=>bullet.y + bullet.width > 0 && bullet.y <= this.canvas.height);
+        console.log(this.bullets.length);
+
+
+        this.bullets.forEach((bullet) => bullet.draw(ctx));
+        if (this.timeTillNextBulletAllowed > 0) {
+            this.timeTillNextBulletAllowed--;
+        }
+    }
+
+    collideWith(sprite) {
+        const bulletThatHitSpriteIndex = this.bullets.findIndex(bullet=>bullet.collideWith(sprite));
+        if(bulletThatHitSpriteIndex >= 0) {
+            this.bullets.splice(bulletThatHitSpriteIndex,1);
+            return true;
+        }
+
+        return false;
+        
+    }
+
+    shoot(x, y, velocity, timeTillNextBulletAllowed = 0) {
+        if(this.timeTillNextBulletAllowed <=0 &&
+            this.bullets.length < this.maxBulletsAtTime) 
+        {
+            const bullet = new Bullet(this.canvas, x, y, velocity, this.bulletColor);
+            this.bullets.push(bullet);
+            if(this.soundEnabled){
+                this.shootsound.currentTime = 0;
+                this.shootsound.play();
+            }
+            this.timeTillNextBulletAllowed = timeTillNextBulletAllowed;
+
+        }
+
+    }
+}     
